699fea82889d2ce51fb34510d2d2c67cf4d67e6a
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-11-01 02:26:02 +0300

    +add: Stickman Animation System

---
+++ Assets/3_Scripts/Stickman Module/AnimationControl/AnimTypes.cs
@@ -0,0 +1,12 @@
+namespace CarLotJam.StickmanModule
+{
+    public enum AnimTypes
+    {
+        IDLE, 
+        HAPPY_IDLE,
+        SAD_IDLE, 
+        RUN,
+        WAVE,
+        ENTER_CAR,
+    }
+}
